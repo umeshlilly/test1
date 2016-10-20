a952e54482617519b77e6056823f59ba926bed09
NickLiffen <UP653591@myport.ac.uk>
2016-10-20 11:14:39 +0100

    Fixed ESLINT Problems - Changed SSO

--- src/web/SSO.js
+++ src/web/SSO.js
@@ -3,12 +3,12 @@ import passport from 'passport';
 const ForceDotComStrategy = require('passport-forcedotcom').Strategy;
 
 /* Gets the enviroment variable from the .env file. If this is set to false then
-	- the user can access the application with no authetication. */
+- the user can access the application with no authetication. */
 const AUTH_REQUIRED = (process.env.AUTH_REQUIRED);
 
 module.exports = (app) => {
 	return {
-		/* initialize the user by logging them in through Force */
+/* initialize the user by logging them in through Force */
 		init() {
 			passport.serializeUser((user, done) => {
 				done(null, user);
@@ -18,7 +18,7 @@ module.exports = (app) => {
 				done(null, obj);
 			});
 
-			// Use the ForceDotComStrategy within Passport
+// Use the ForceDotComStrategy within Passport
 			const sfStrategy = new ForceDotComStrategy({
 				clientID: process.env.CF_CLIENT_ID,
 				clientSecret: process.env.CF_CLIENT_SECRET,
@@ -27,8 +27,10 @@ module.exports = (app) => {
 				tokenURL: process.env.SF_TOKEN_URL,
 				profileFields: ['user_id', 'first_name'],
 			}, (accessToken, refreshToken, profile, done) => {
-				// Only retain the profile properties we need.
-				profile.user_id = profile._raw.user_id;
+// Only retain the profile properties we need.
+
+/* This may change */
+				profile.user_id = profile.raw.user_id;
 				return done(null, profile);
 			});
 
@@ -38,7 +40,7 @@ module.exports = (app) => {
 			app.use(passport.session());
 		},
 
-		/* This function includes all the needed routes for authetication.*/
+/* This function includes all the needed routes for authetication.*/
 		registerRoutes() {
 			app.get('/auth/forcedotcom', (req, res, next) => {
 				if (req.query.redirect) {
@@ -47,7 +49,7 @@ module.exports = (app) => {
 				passport.authenticate('forcedotcom')(req, res, next);
 			});
 
-			/* Callback route for when the data comes back from the '/auth/forcedotcom Route' */
+/* Callback route for when the data comes back from the '/auth/forcedotcom Route' */
 			app.get('/auth/forcedotcom/callback', passport.authenticate('forcedotcom', {
 				failureRedirect: '/error',
 			}), (req, res) => {
@@ -58,9 +60,9 @@ module.exports = (app) => {
 				res.redirect(303, redirect || '/');
 			});
 
-			/* If there is no authentication then it automatically goes to the index page
-			- if autheication is set to true it then goes through the method of trying to
-			- autheticate */
+/* If there is no authentication then it automatically goes to the index page
+- if autheication is set to true it then goes through the method of trying to
+- autheticate */
 			app.get('/', (req, res) => {
 				if ((!req.user) && (AUTH_REQUIRED === 'true')) {
 					if (req.session) {
@@ -72,7 +74,7 @@ module.exports = (app) => {
 				return res.render('index');
 			});
 
-			/* Route for when the user logs out.*/
+/* Route for when the user logs out.*/
 			app.get('/logout', (req, res) => {
 				req.logout();
 				req.session.destroy();
@@ -80,9 +82,9 @@ module.exports = (app) => {
 			});
 		},
 
-		/* This is a function to check and make sure the user is authetnicated. If in the
-		- .env AUTH is set to false it skips and carrys on. If AUTH is set to true it runs
-		- and make sure the user is autheicated. */
+/* This is a function to check and make sure the user is authetnicated. If in the
+- .env AUTH is set to false it skips and carrys on. If AUTH is set to true it runs
+- and make sure the user is autheicated. */
 		ensureAuthenticated(req, res, next) {
 			if (req.isAuthenticated() || !AUTH_REQUIRED) {
 				return next();
