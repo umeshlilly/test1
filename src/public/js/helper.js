5905f25e909410a176e98fa9e76ae1047884ed59
NickLiffen <UP653591@myport.ac.uk>
2017-01-04 08:59:47 +0000

    Fixed Auth

--- src/public/js/helper.js
+++ src/public/js/helper.js
@@ -4,33 +4,33 @@ module.exports = {
 into two arrays [a, b]. It counts how many times a value is in the array and seperates it
 to the name and the amount of times it appears.
 The output would be ['Nick', 'Bob'] [2, 1]. */
-	order: (arr) => {
-		const a = [];
-		const	b = [];
-		let	prev;
+  order: (arr) => {
+    const a = [];
+    const	b = [];
+    let	prev;
 
-		arr.sort();
-		for (let i = 0; i < arr.length; i++) {
-			if (arr[i] !== prev) {
-				a.push(arr[i]);
-				b.push(1);
-			} else {
-				b[b.length - 1]++;
-			}
-			prev = arr[i];
-		}
-		return [a, b];
-	},
+    arr.sort();
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== prev) {
+        a.push(arr[i]);
+        b.push(1);
+      } else {
+        b[b.length - 1]++;
+      }
+      prev = arr[i];
+    }
+    return [a, b];
+  },
 
-  /* This helper function checks for an empty object. Return true if empty, returns
-     false if not empty*/
-	objectEmpty: (obj) => {
-		let value;
-		if (Object.keys(obj).length === 0 && obj.constructor === Object) {
-			value = true;
-		} else {
-			value = false;
-		}
-		return value;
-	},
+/* This helper function checks for an empty object. Return true if empty, returns
+false if not empty*/
+  objectEmpty: (obj) => {
+    let value;
+    if (Object.keys(obj).length === 0 && obj.constructor === Object) {
+      value = true;
+    } else {
+      value = false;
+    }
+    return value;
+  },
 };
--- src/routes/authenticated-routes.js
+++ src/routes/authenticated-routes.js
@@ -11,22 +11,22 @@ const AUTH_REQUIRED = (process.env.AUTH_REQUIRED === 'true');
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
-	if (req.isAuthenticated() || !AUTH_REQUIRED) {
-		return next();
-	}
-	return res.redirect(`/auth/forcedotcom?redirect=${req.originalUrl}`);
+  if (req.isAuthenticated() || !AUTH_REQUIRED) {
+    return next();
+  }
+  return res.redirect(`/auth/forcedotcom?redirect=${req.originalUrl}`);
 });
 
 router.get('/about', (req, res) => {
-	testFile.test().then((data) => {
-		return res.render('about', {
-			title: data,
-		});
-	}).catch((e) => {
-		res.status(500, {
-			error: e,
-		});
-	});
+  testFile.test().then((data) => {
+    return res.render('about', {
+      title: data,
+    });
+  }).catch((e) => {
+    res.status(500, {
+      error: e,
+    });
+  });
 });
 
 
