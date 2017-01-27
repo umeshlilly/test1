089d934315701913a93bcaa179f4fb078382c27a
NickLiffen <UP653591@myport.ac.uk>
2017-01-27 08:27:28 +0000

    Adding Better Heroku CI Support

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-
 /* These are the required packages needed to carry out the Gulp Task Below. */
 const gulp = require('gulp');
 const source = require('vinyl-source-stream');
@@ -8,71 +7,83 @@ const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
+const uglify = require('gulp-uglify');
+const cleanCSS = require('gulp-clean-css');
+const pump = require('pump');
 
 
 // This is the bebel task and trasnforms all my ES2016 back to ES5 so all browers can read
 gulp.task('babelify', () => {
-  return gulp.src('src/**/**/*.js')
-.pipe(babel({
-  presets: ['es2015'],
-}))
-.pipe(gulp.dest('lib'));
+  return gulp.src('src/**/**/*.js').pipe(babel({
+    presets: ['es2015'],
+  })).pipe(gulp.dest('lib'));
 });
 
 // This is the browserify task.
 gulp.task('browserify', () => {
   return browserify('./lib/public/js/main.js')
-.transform('babelify')
-.bundle()
-.pipe(source('bundle.js')) // this is the output file name
-.pipe(gulp.dest('./lib/public/js/')); // and this is where it ends up
+        .transform('babelify')
+        .bundle()
+        .pipe(source('bundle.js')) // this is the output file name
+        .pipe(gulp.dest('./lib/public/js/')); // and this is where it ends up
 });
 
 // This is the imaging task.
 gulp.task('image', () => {
   return gulp.src('./src/public/img/*')
-.pipe(imagemin())
-.pipe(gulp.dest('lib/public/img/'));
+        .pipe(imagemin())
+        .pipe(gulp.dest('lib/public/img/'));
 });
 
 // This task transforms SASS into CSS.
 gulp.task('sass', () => {
   return gulp.src('./src/public/scss/*.scss')
-.pipe(sass().on('error', sass.logError))
-.pipe(gulp.dest('./lib/public/css'));
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./lib/public/css'));
 });
 
 // This task runs unit testing from Mocha.
 gulp.task('mocha', () => {
-  gulp.src('tests/mocha.js', { read: false })
-// gulp-mocha needs filepaths so you can't have any plugins before it
-.pipe(mocha({ reporter: 'nyan' }));
+  gulp.src('tests/mocha.js', {
+    read: false,
+  }).pipe(mocha({
+    reporter: 'nyan',
+  }));
 });
 
+// This task runs the standards check agsinst the `.eslintrc` file in the root directory.
 gulp.task('lint', () => {
-// ESLint ignores files with "node_modules" paths.
-// So, it's best to have gulp ignore the directory as well.
-// Also, Be sure to return the stream from the task;
-// Otherwise, the task may end before the stream has finished.
-  return gulp.src(['**/*.js', '!node_modules/**', '!lib/**', '!src/public/js/bundle.js'])
-// eslint() attaches the lint output to the "eslint" property
-// of the file object so it can be used by other modules.
-.pipe(eslint())
-// eslint.format() outputs the lint results to the console.
-// Alternatively use eslint.formatEach() (see Docs).
-.pipe(eslint.format())
-// To have the process exit with an error code (1) on
-// lint error, return the stream and pipe to failAfterError last.
-.pipe(eslint.failAfterError());
+  return gulp.src(['**/*.js', '!node_modules/**', '!lib/**', '!src/public/js/bundle.js', '!src/public/vendor/**'])
+        .pipe(eslint()).pipe(eslint.format()).pipe(eslint.failAfterError());
+});
+
+// This task compresses vendor javascript code.
+gulp.task('minify-js', () => {
+  pump([
+    gulp.src('src/public/vendor/*.js'),
+    uglify(),
+    gulp.dest('lib/public/vendor'),
+  ]);
+});
+
+// This task compresses vendor css code.
+gulp.task('minify-css', () => {
+  return gulp.src('src/public/vendor/*.css')
+        .pipe(cleanCSS({
+          compatibility: 'ie8',
+        }))
+        .pipe(gulp.dest('lib/public/vendor'));
 });
 
 
-// THIS IS MY DEFAULT TASK - Needs to watch both folder ([Folders / Files to Watch], [Gulp Task])
+// THIS IS THE DEFAULT TASK - Needs to watch both folder ([Folders / Files to Watch], [Gulp Task])
 gulp.task('watch', () => {
-  gulp.watch(['./src/**/*.js'], ['babelify']);
+  gulp.watch(['./src/controllers/*.js'], ['babelify']);
   gulp.watch(['./src/public/img/*'], ['image']);
   gulp.watch(['./src/public/scss/*.scss'], ['sass']);
   gulp.watch(['./lib/public/js/*.js'], ['browserify']);
+  gulp.watch(['./src/public/vendor/*.js'], ['minify-js']);
+  gulp.watch(['./src/public/vendor/*.css'], ['minify-css']);
 });
 
 // Make my default task to watch both folders
